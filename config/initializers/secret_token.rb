3f57fc3986c1179e7bc74ec2601d993218c81dcb
Siddhant Chothe <sidnc86@gmail.com>
2013-04-22 12:26:13 +0530

    Adding the basic rails application to the repository to be able to work on the prototype.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AudioInteractivityPrototype::Application.config.secret_token = '16d8e5095d8562893d3606b49e04287ab4268e197d161376c04ecfeadaadc261ded9b0840024f9a931c5ae01ace02a83b7ba340231871d4e3bc4cb6ce3c76dce'
